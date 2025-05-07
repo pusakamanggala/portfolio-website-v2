c2dff10f650a6a42dfe1ae046f33930fe1cfadfa
Pusaka Manggala <pusakamanggala@gmail.com>
2025-05-07 15:10:35 +0700

    feat : experience

---
+++ src/components/sections/experience.tsx
@@ -0,0 +1,47 @@
+export default function Experience() {
+  return (
+    <section id="experience">
+      <h2 className="section-title">EXPERIENCE</h2>
+      <div>
+        <h3 className="font-semibold">
+          Front-end Developer • PT Awan Dinata Teknologi
+        </h3>
+        <p className="text-gray-400 text-sm">DECEMBER 2023 - PRESENT</p>
+        <ul className="list-disc ml-4 space-y-4 mt-5 experience-list">
+          <li>
+            <h3 className="text-base">
+              Eka Jaya Berindo Driving School System
+            </h3>
+            <p className="text-gray-400 mt-1 font-light">
+              Developed a comprehensive application system for Eka Jaya Berindo
+              to streamline driving lesson appointments, accompanied by a CMS
+              dashboard for efficient data and activity management. The system
+              facilitates seamless coordination between students, instructors,
+              and administrators, ensuring a structured and user-friendly
+              experience. For instructors, the app provides functionalities to
+              view assigned classes (appointments), manage car availability, and
+              report incidents during lessons. Students can check their
+              appointment schedules, submit feedback, track their driving lesson
+              progress, and purchase lesson packages. Meanwhile, administrators
+              have full control over managing student and instructor profiles,
+              scheduling classes, overseeing car allocations, and handling
+              driving lesson packages.
+            </p>
+          </li>
+          <li>
+            <h3 className="text-base">Persib Bandung Digital Platforms</h3>
+            <p className="text-gray-400 mt-1 font-light">
+              Developed new features and implemented redesigns across multiple
+              Persib websites, including PlanetPersib.com and Persib.co.id,
+              enhancing functionality, user experience and ensured better
+              engagement. Key contributions included building a comprehensive
+              article system with a dedicated CMS dashboard for streamlined
+              content management on PlanetPersib.com, as well as creating an
+              installment-based membership payment.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
